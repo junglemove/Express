ced1c2599029f8ceb5c335ce0484ad791e8e864b
junglemove <thibaut.visticot@gmail.com>
2016-12-14 13:13:39 +0100

    Middleware 404

---
+++ middleware.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Administrateur on 14/12/2016.
+ */
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res, next) => {
+    res.send('<h2>Acceuil</h2>');
+});
+
+server.get('/contacts', (req, res, next) => {
+    res.send('<h2>Liste des contacts</h2>');
+});
+
+server.get('/contacts:id', (req, res, next) => {
+    var id = parseInt(req.params.id);
+
+    if(isNaN(id)){
+        return next();
+    }
+
+    res.send('<h2>Liste des contacts</h2>');
+});
+
+server.all('/*',(req, res, next) => {
+    res.statusCode = 404;
+    res.send('<h2>Not found</h2>');
+});
+
+
+server.listen(8080, () => {
+    console.log("Le serveur écoute sur le port 8080");
+});
